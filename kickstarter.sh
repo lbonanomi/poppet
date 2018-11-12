eed13a7c2d74230a6502eae2aa4d5a85e69650eb
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2018-11-12 10:46:12 -0500

    Update kickstarter.sh

--- kickstarter.sh
+++ kickstarter.sh
@@ -38,12 +38,25 @@ su -c "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /
 #
 su -c "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql -c \"GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USERNAME;\"" postgres
 
-
-# Lock and load
+# Load dump to DB
 #
 curl -s $DBDUMP | LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql -U $DB_USERNAME $DB_NAME
 
+# Disable all directories except the base, local directory
+#
 su -c "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql  -U $DB_USERNAME $DB_NAME -c \"update cwd_directory  set active = 0 where id != 1;\"" postgres
+
+
+# Reset passwords for all members of 'jira-administrators' group
+#
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql -U $DB_USERNAME $DB_NAME -t -c "select child_name from cwd_membership where parent_name='jira-administrators' AND directory_id = 1;" | grep -v "^$" | while read localadmin;
+do
+        echo $localadmin;
+        LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql -U $DB_USERNAME $DB_NAME -t -c "update cwd_user set credential='{PKCS5S2}b3c19ePbQB4BAWzb6NogB7oTuSKOATvJxT1JP/1knh+fi1ZwJ8TGmnzmssJsBYvG' where user_name='"$localadmin"';"
+done
+
+
+#####################
 
 # What version of Jira was this backed-up from? 
 JIRA_VERSION=$(LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-postgresql10/root/usr/lib64 /opt/rh/rh-postgresql10/root/usr/bin/psql -t -U postgres jiraprdb1 -c "select propertyvalue from propertystring where id = (select id from propertyentry where property_key = 'jira.version');" | tr -d ' ')
